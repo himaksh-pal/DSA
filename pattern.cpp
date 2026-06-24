6c806cf1df1f8afc571ca82b1d828343bffe57a4
himaksh-pal <palhimaksh@gmail.com>
2026-06-24 13:12:24 +0530

    Initial commit

---
+++ pattern.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+using namespace std;
+void pattern8(int n){
+    for(int i=0;i<n;i++ ){
+            //space
+            for(int j=0;j<i;j++){
+                cout<<" ";
+            }
+            //stars
+            for(int j=0;j<2*(n-i)-1;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+}
+void pattern9(int n){
+    
+        for(int i =1;i<=n;i++){
+            //space
+            for(int j=1;j<=n-i;j++){
+                cout<<" ";
+            }
+            //stars
+            for(int j=1; j<=(2*i)-1;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+        for(int i=0;i<n;i++ ){ 
+            //space
+            for(int j=0;j<i;j++){
+                cout<<" ";
+            }
+            //stars
+            for(int j=0;j<2*(n-i)-1;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+        
+}
+void pattern10(int n){
+    for(int i=1;i<=2*n-1;i++){
+        int stars = i;
+        if(i>n)stars=2*n-i;
+        for(int j=1;j<=stars;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+void pattern11(int n){
+    int start=1;
+    for(int i=1;i<=n;i++){
+        if(i%2==0)start=0;
+        else start=1;
+        for(int j=1;j<=i;j++){
+            cout<<start;
+            start=1-start;
+        }
+        cout<<endl;
+    }
+}
+void pattern12(int n){
+    for(int i=1;i<=n;i++){
+        //num
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        //space
+        for(int j=1;j<=2*(n-i);j++){
+            cout<<" ";
+        }
+        //num
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    
+}
+int main(){
+    int t;
+    cin>>t;
+    int n1;
+    for(int i=0;i<t;i++){
+        cin>>n1;
+        pattern12(n1);
+    }
+
+    return 0;
+}
