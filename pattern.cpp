1ec5a5458e8eeffd6e3a14d93b0044a417a29109
himaksh-pal <palhimaksh@gmail.com>
2026-06-25 15:20:46 +0530

    C++

--- pattern.cpp
+++ pattern.cpp
@@ -153,7 +153,7 @@ void pattern19(int n){
             cout<<"*";
         }
         //space
-        for(int j =0 ;j<=spaces;j++){
+        for(int j =1 ;j<=spaces;j++){
             cout<<" ";
         }
         //star
@@ -163,6 +163,75 @@ void pattern19(int n){
         cout<<endl;
         spaces=spaces+2;
     }
+
+
+    int spaces1 =spaces-2;
+    for(int i=1;i<=n;i++){
+        
+        //star
+        for(int j = 1; j<=i;j++){
+            cout<<"*";
+        }
+        //space
+        for(int j =1 ;j<=spaces1;j++){
+            cout<<" ";
+        }
+        //star
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+        spaces1=spaces1-2;
+    }
+}
+void pattern20(int n){
+    int spaces=2*n-2;
+    for(int i=1;i<=2*n-1;i++){
+        int star=i;
+        if(i>n){
+            star=2*n-i;
+        }
+        for(int j =1;j<=star;j++){
+            cout<<"*";
+        }
+        for(int j=1;j<=spaces;j++){
+            cout<<" ";
+        }
+        for(int j =1;j<=star;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+        if(i<n)
+        spaces-=2; 
+        else
+        spaces+=2;
+    }
+}
+void pattern21(int n){
+    for(int i=0;i<n;i++){
+        for (int j = 0; j < n; j++)
+        {
+            if( i ==0 || j==0 || i == n-1 || j== n-1 ){
+                cout<<"*";
+            }    
+            else cout<<" ";
+        }
+        cout<<endl;
+        
+    }
+}
+void pattern22(int n){
+    for(int i=0;i<2*n-1;i++){
+        for(int j=0;j<2*n-1;j++){
+            int top = i;
+            int left = j;
+            int right = (2*n-2)-j;
+            int bottom = (2*n-2)-i;
+            cout<<(n-min(min(top,bottom),min(left,right)))<<" ";
+        }
+        cout<<endl;
+    }
+
 }
 int main(){
     int t;
@@ -170,7 +239,7 @@ int main(){
     int n1;
     for(int i=0;i<t;i++){
         cin>>n1;
-        pattern19(n1);
+        pattern22(n1);
     }
 
     return 0;
