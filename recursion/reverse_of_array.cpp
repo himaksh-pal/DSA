d02774601073b636f53350ba2cd581bb949d51cd
himaksh-pal <palhimaksh@gmail.com>
2026-07-21 17:14:18 +0530

    C++

---
+++ recursion/reverse_of_array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void reverse(int l, int r){
+    if(l>=r)return;
+    swap()
+
+        
+    }
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    reverse(0,n-1);
+    return 0;
+}
--- recursion/sum_of_n.cpp
+++ recursion/sum_of_n.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 using namespace std;
-void print(int i,int n){
-    i
+int print(int N){                          // functional way
+            if(N==0)
+            return 0;
+            return N+print(N-1);
+		}
 
+void print1(int n, int sum){              // parameterised way
+    if(n<1){
+    cout<<sum<<endl;
+    return ;
+}
+    print1(n-1,sum+n);
 }
 int main(){
     int n;
     cin>>n;
-    print(1,n);
+    print1(n,0);
     return 0;
 }
