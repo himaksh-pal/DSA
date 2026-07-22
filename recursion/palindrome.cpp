55b524a70d2602dc754eca44433cea86b3aeb5ad
himaksh-pal <palhimaksh@gmail.com>
2026-07-22 12:24:41 +0530

    C++

---
+++ recursion/palindrome.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+bool palindromeCheck(string& ){
+
+	}
+bool palindrome(int i,int arr[],int n){
+    if(i<=1){
+    }
+}    
+int main(){
+    return 0;
+}
--- recursion/reverse_of_array.cpp
+++ recursion/reverse_of_array.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 using namespace std;
-void reverse(int l, int r){
-    if(l>=r)return;
-    swap()
+void reverse(int i,int arr[],int n){
+    if(i>=n/2)return;
+    swap(arr[i],arr[n-i-1]);
+    reverse(i+1,arr,n);
 
+        
+    }
+
+ void reverse_s(int arr[], int n){               //according to the sheet
+        if(n<=1)return;
+        swap(arr[0],arr[n-1]);
+        reverse_s(arr+1,n-2);
         
     }
 int main(){
     int n;
     cin>>n;
     int arr[n];
-    reverse(0,n-1);
+    for(int i=0;i<n;i++)cin>>arr[i];
+    reverse_s(arr,n);
+    for(int i=0;i<n;i++)cout<<arr[i]<<" " ;
     return 0;
 }
