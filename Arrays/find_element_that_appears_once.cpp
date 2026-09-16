6c01d03861e5ead058594f8e0864571cf1572e97
himaksh-pal <palhimaksh@gmail.com>
2026-09-16 22:22:03 +0530

    C++

---
+++ Arrays/find_element_that_appears_once.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int singleNumberMB(vector<int>& nums){  // brute , MY
+        int n = nums.size();
+        int target;
+        for(int i=0;i<n;i++){
+            int cnt =0;
+            for(int j=0;j<n;j++){
+                if(nums[i]==nums[j]){
+                    cnt++;
+                }
+                if(cnt==2){
+                    break;
+                }
+            }
+            if(cnt==1){
+                target =nums[i];
+                break;
+            }
+        }
+        return target;
+}
+int singleNumber(vector<int>& nums){ // optimal
+
+}
+int main(){
+    cout<<"enter size of the vector :";
+    int n ;
+    cin>>n;
+    vector<int> nums(n);
+    cout<<"Enter the elements of the vector :";
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    int result = singleNumber(nums);
+    cout<<" RESULT : "<<result;
+    return 0;
+}
